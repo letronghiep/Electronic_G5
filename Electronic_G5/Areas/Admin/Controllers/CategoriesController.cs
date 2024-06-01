516e8994f5dfcbd1a8af67050152e83dd92f4503
letronghiep <letronghiep213@gmail.com>
2024-06-01 21:31:29 +0700

    ignore app_data

---
+++ Electronic_G5/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Electronic_G5.Models;
+
+namespace Electronic_G5.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ElectronicDb db = new ElectronicDb();
+
+        // GET: Admin/Categories
+        public ActionResult Index()
+        {
+            var categories = db.Categories.Include(c => c.Category1);
+            return View(categories.ToList());
+        }
+
+        // GET: Admin/Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Create
+        public ActionResult Create()
+        {
+            ViewBag.parent_id = new SelectList(db.Categories, "category_id", "category_name");
+            return View();
+        }
+
+        // POST: Admin/Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "category_id,category_name,parent_id,created_at,updated_at")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.created_at = DateTime.Now;
+                category.updated_at = DateTime.Now;
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.parent_id = new SelectList(db.Categories, "category_id", "category_name", category.parent_id);
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.parent_id = new SelectList(db.Categories, "category_id", "category_name", category.parent_id);
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "category_id,category_name,parent_id,created_at,updated_at")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.updated_at = DateTime.Now;
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.parent_id = new SelectList(db.Categories, "category_id", "category_name", category.parent_id);
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
