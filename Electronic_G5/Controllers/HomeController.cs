32404e874a93a471a182dc8b24f57b7286a5424e
phuong520 <phuongminzy2@gmail.com>
2024-06-15 22:52:39 +0700

    find by name/price

--- Electronic_G5/Controllers/HomeController.cs
+++ Electronic_G5/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -17,18 +18,31 @@ namespace Electronic_G5.Controllers
 
         [AllowAnonymous]
 
-        public ActionResult Index(string filter)
+        public ActionResult Index(string filter, string search)
         {
+            System.Diagnostics.Debug.WriteLine($"Filter: {filter}, Search: {search}");
             List<ProductViewModel> products = new List<ProductViewModel>();
+            var query = db.Products.AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                decimal searchPrice;
+                bool isPrice = decimal.TryParse(search, out searchPrice);
+
+                if (isPrice)
+                {
+                    query = query.Where(p => p.price == searchPrice);
+                }
+                else
+                {
+                    query = query.Where(p => p.product_name.Contains(search));
+                }
+            }
+
             switch (filter)
             {
                 case "latest":
-                    // Hiển thị danh sách sản phẩm mới nhất
-                    products = db.Products.OrderByDescending(p => p.created_at).Select(p => new ProductViewModel
-                    {
-                        Product = p,
-                        Images = db.Images.Where(i => i.product_id == p.product_id).ToList()
-                    }).ToList();
+                    query = query.OrderByDescending(p => p.created_at);
                     break;
 
                 case "bestselling":
@@ -38,27 +52,24 @@ namespace Electronic_G5.Controllers
                         product_id = g.Key,
                         order_quantity = g.Sum(oi => oi.quantity),
                         product = g.FirstOrDefault().Product
-                    }).OrderByDescending(g => g.order_quantity).ToList();
+                    }).OrderByDescending(g => g.order_quantity).Select(g => g.product);
 
-                    products = topSalesProducts.Select(g => new ProductViewModel
-                    {
-                        Product = g.product,
-                        Images = db.Images.Where(i => i.product_id == g.product.product_id).ToList()
-                    }).ToList();
+                    query = query.Where(p => topSalesProducts.Any(tsp => tsp.product_id == p.product_id));
                     break;
-                    //sort
-                    
+                //sort
+
 
                 default:
-                    // Hiển thị tất cả sản phẩm
-                    products = db.Products.Select(p => new ProductViewModel
-                    {
-                        Product = p,
-                        Images = db.Images.Where(i => i.product_id == p.product_id).ToList()
-                    }).ToList();
                     break;
             }
+            System.Diagnostics.Debug.WriteLine("Query:" + query);
+            var productList = query.ToList(); // Chuyển đổi thành danh sách trước
 
+            products = productList.Select(p => new ProductViewModel
+            {
+                Product = p,
+                Images = db.Images.Where(i => i.product_id == p.product_id).ToList()
+            }).ToList();
             //hiển thị lên view
             var viewModel = new HomeViewModel
             {
--- Electronic_G5/Models/HomeViewModel.cs
+++ Electronic_G5/Models/HomeViewModel.cs
@@ -9,6 +9,7 @@ namespace Electronic_G5.Models
     {
         public List<ProductViewModel> Products { get; set; } = new List<ProductViewModel>();
         public List<ProductViewModel> LatestProducts { get; set; } = new List<ProductViewModel>();
+        public List<Cart> cart { get; set; } = new List<Cart>();
      //   public List<BestSelling> TopSalesProducts { get; set; } = new List<BestSelling> ();
 
     }
